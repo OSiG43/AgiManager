5e9b9d191a7a7900c674e7ead63cba81a8731a17
BdB <quentin.lo-pinto@gadz.org>
2023-03-24 15:55:20 +0100

    commit vide

--- agimanager/agilog/requests.py
+++ agimanager/agilog/requests.py
@@ -1,2 +1,11 @@
 # Contient toutes les fonctions qui feront des requetes concernant agilog à la base de données
+import sqlite3 as lite
 
+
+def getStock():
+    con=lite.connect('BDD_OREXA.db')
+    con.row_factory=lite.Row
+    cur=con.cursor()
+    cur.execute('SELECT ID_piece, Quantité FROM Stock Agilog')
+    lignes=cur.fetchall()
+    con.close()
