b8df7d5f29e1334995ec2e13faf70569583de7a8
BoisdeBool-On <quentin.lo-pinto@gadz.org>
2023-04-17 14:35:32 +0200

    getPieceList getKitList getPieceKit

--- agimanager/agilog/requests.py
+++ agimanager/agilog/requests.py
@@ -1,11 +1,31 @@
 # Contient toutes les fonctions qui feront des requetes concernant agilog à la base de données
 import sqlite3 as lite
+from agimanager.db_utils import get_db
 
 
-def getStock():
-    con=lite.connect('BDD_OREXA.db')
-    con.row_factory=lite.Row
+def getAllStock():
+    con= get_db()
     cur=con.cursor()
-    cur.execute('SELECT ID_piece, Quantité FROM Stock Agilog')
+    cur.execute('SELECT ID_piece, Quantité FROM Stock_Agilog')
     lignes=cur.fetchall()
-    con.close()
+
+def getPieceList() :
+    con = get_db()
+    cur = con.cursor()
+    cur.execute('SELECT id, ref, nom_piece FROM Pieces')
+    lignes = cur.fetchall()
+
+def getKitList():
+    con = get_db()
+    cur = con.cursor()
+    cur.execute('SELECT id, nom_kit FROM Kits')
+    lignes = cur.fetchall()
+
+def getPiecesKit(id_kit) :
+    con = get_db()
+    cur = con.cursor()
+    cur.execute(f"SELECT id_kit, id_piece, quantié FROM Composition_kit WHERE id_kit = {id_kit}")
+    lignes = cur.fetchall()
+
+
+
